da6e09b71a809fcdc47ba0c72eb8f7d57ede734e
arksohail <arksohail@gmail.com>
2024-10-01 14:50:24 +0530

    Working with Nested Form Groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,14 +11,18 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class SignupComponent {
   form = new FormGroup({
     email: new FormControl("", { validators: [Validators.email, Validators.required] }),
-    password: new FormControl("", { validators: [Validators.required, Validators.minLength(6)] }),
-    confirmPassword: new FormControl("", { validators: [Validators.required, Validators.minLength(6)] }),
+    passwords: new FormGroup({
+      password: new FormControl("", { validators: [Validators.required, Validators.minLength(6)] }),
+      confirmPassword: new FormControl("", { validators: [Validators.required, Validators.minLength(6)] }),
+    }),
     firstName: new FormControl("", { validators: [Validators.required] }),
     lastName: new FormControl("", { validators: [Validators.required] }),
-    street: new FormControl("", { validators: [Validators.required] }),
-    number: new FormControl("", { validators: [Validators.required] }),
-    postalCode: new FormControl("", { validators: [Validators.required] }),
-    city: new FormControl("", { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl("", { validators: [Validators.required] }),
+      number: new FormControl("", { validators: [Validators.required] }),
+      postalCode: new FormControl("", { validators: [Validators.required] }),
+      city: new FormControl("", { validators: [Validators.required] }),
+    }),
     role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>("student", { validators: [Validators.required] }),
     agree: new FormControl<boolean>(false, { validators: [Validators.required] }),
   });
